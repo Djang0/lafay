6362da718d1615443f59f14270186b6ef9462b04
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2013-06-19 12:56:31 +0200

    finishing permission ui

--- lafay/src/be/lreenaers/lafay/web/control/PermissionCtrl.java
+++ lafay/src/be/lreenaers/lafay/web/control/PermissionCtrl.java
@@ -3,6 +3,8 @@
  */
 package be.lreenaers.lafay.web.control;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.model.ListDataModel;
@@ -13,6 +15,7 @@ import be.lreenaers.lafay.DAOs.PermissionDAO;
 import be.lreenaers.lafay.beans.Permission;
 import be.lreenaers.lafay.factories.DAOFactory;
 import be.lreenaers.lafay.web.interfaces.Controlable;
+import be.lreenaers.lafay.web.interfaces.Filterable;
 import be.lreenaers.lafay.web.interfaces.RowEditable;
 
 /**
@@ -21,19 +24,22 @@ import be.lreenaers.lafay.web.interfaces.RowEditable;
  */
 @ManagedBean(name="permissionCtrl")
 @SessionScoped
-public class PermissionCtrl implements Controlable, RowEditable{
+public class PermissionCtrl implements Controlable, RowEditable, Filterable<Permission>{
 	// TODO:  implements Controlable, RowEditable for other COntrolers
 	private PermissionDAO dao;
 	private ListDataModel<Permission> permissions;
 	private Permission permission;
 	private Permission permissionEdit;
+	private List<Permission> filtered;
 	
+
 	public PermissionCtrl(){
 		this.dao = DAOFactory.getPermissionDAO();
 		this.permissions = new ListDataModel<Permission>();
 		this.permissions.setWrappedData(this.dao.all());
 		this.permission = new Permission();
 		this.permissionEdit = new Permission();
+		
 	}
 	public String delete(){
 		Permission p = (Permission) permissions.getRowData();
@@ -59,7 +65,9 @@ public class PermissionCtrl implements Controlable, RowEditable{
 	public void onCancel(RowEditEvent event){
 		
 	}
-	
+	 public String goCreate(){
+		 return "goCreatePerm";
+	 }
 	
 	public PermissionDAO getDao() {
 		return dao;
@@ -86,5 +94,10 @@ public class PermissionCtrl implements Controlable, RowEditable{
 		this.permissionEdit = permissionEdit;
 	}
 	
-	
+	public List<Permission> getFiltered() {
+		return filtered;
+	}
+	public void setFiltered(List<Permission> filtered) {
+		this.filtered = filtered;
+	}
 }
