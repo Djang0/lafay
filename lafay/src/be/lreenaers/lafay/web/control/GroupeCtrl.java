cf148b1d158d469263a3f7b1408dceca73b8cc75
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2013-06-26 09:30:37 +0200

    - on groupe delete, remove reference from parent utilisateur

--- lafay/src/be/lreenaers/lafay/web/control/GroupeCtrl.java
+++ lafay/src/be/lreenaers/lafay/web/control/GroupeCtrl.java
@@ -14,18 +14,22 @@ import org.primefaces.model.DualListModel;
 
 import be.lreenaers.lafay.DAOs.GroupeDAO;
 import be.lreenaers.lafay.DAOs.PermissionDAO;
+import be.lreenaers.lafay.DAOs.UtilisateurDAO;
 import be.lreenaers.lafay.beans.Groupe;
 import be.lreenaers.lafay.beans.Permission;
+import be.lreenaers.lafay.beans.Utilisateur;
 import be.lreenaers.lafay.factories.DAOFactory;
 import be.lreenaers.lafay.web.interfaces.Controlable;
 import be.lreenaers.lafay.web.interfaces.Filterable;
 import be.lreenaers.lafay.web.interfaces.Modifiable;
 import be.lreenaers.lafay.web.interfaces.RowEditable;
 
-@ManagedBean(name="groupeCtrl")
+@ManagedBean(name = "groupeCtrl")
 @SessionScoped
-public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>, Modifiable{
+public class GroupeCtrl implements Controlable, RowEditable,
+		Filterable<Groupe>, Modifiable {
 	private GroupeDAO dao;
+	private UtilisateurDAO udao;
 	private PermissionDAO pdao;
 	private List<Groupe> filtered;
 	private Groupe groupeEdit;
@@ -33,7 +37,74 @@ public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>,
 	private ListDataModel<Groupe> groupes;
 	private DualListModel<Permission> pickCreatePerms;
 	private DualListModel<Permission> pickEditPerms;
-	
+
+	public GroupeCtrl() {
+		this.dao = DAOFactory.getGroupeDAO();
+		this.pdao = DAOFactory.getPermissionDAO();
+		this.groupe = new Groupe();
+		this.groupeEdit = new Groupe();
+		this.groupes = new ListDataModel<Groupe>();
+		this.groupes.setWrappedData(dao.all());
+
+	}
+
+	@Override
+	public String create() {
+		Iterator<Permission> i = this.groupe.getPermissions().iterator();
+		while (i.hasNext()) {
+			Permission permission = i.next();
+			System.out.println("saving:" + permission.getId().toString() + "//"
+					+ permission.getValeur());
+		}
+		this.dao.save(this.groupe);
+		this.groupe = new Groupe();
+		this.groupes.setWrappedData(dao.all());
+		return "goGrpList";
+	}
+
+	@Override
+	public String delete() {
+		Groupe g = (Groupe) this.groupes.getRowData();
+		this.udao = DAOFactory.getUtilisateurDAO();
+		List<Utilisateur> usrs = this.udao.all();
+		Iterator<Utilisateur> it = usrs.iterator();
+		Utilisateur usr;
+		List<Groupe> grps;
+		while(it.hasNext()){
+			usr = it.next();
+			grps = usr.getGroups();
+			grps.remove(g);
+			usr.setGroups(grps);
+			this.udao.save(usr);
+		}
+		this.dao.delete(g);
+		this.groupes.setWrappedData(dao.all());
+		return "goGrpList";
+	}
+
+	@Override
+	public String edit() {
+		this.groupeEdit = (Groupe) this.groupes.getRowData();
+		return "goModGroup";
+	}
+
+	@Override
+	public List<Groupe> getFiltered() {
+		return this.filtered;
+	}
+
+	public Groupe getGroupe() {
+		return groupe;
+	}
+
+	public Groupe getGroupeEdit() {
+		return groupeEdit;
+	}
+
+	public ListDataModel<Groupe> getGroupes() {
+		return groupes;
+	}
+
 	public DualListModel<Permission> getPickCreatePerms() {
 		List<Permission> target = new ArrayList<Permission>();
 		List<Permission> source = this.pdao.all();
@@ -41,12 +112,7 @@ public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>,
 		this.pickCreatePerms = new DualListModel<Permission>(source, target);
 		return this.pickCreatePerms;
 	}
-	public void setPickCreatePerms(DualListModel<Permission> pickCreatePerms) {
-		this.pickCreatePerms = pickCreatePerms;
-	}
-	public String goCreate(){
-		return "goCreateGroup";
-	}
+
 	public DualListModel<Permission> getPickEditPerms() {
 		List<Permission> target = this.groupeEdit.getPermissions();
 		List<Permission> source = this.pdao.all();
@@ -54,44 +120,15 @@ public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>,
 		this.pickEditPerms = new DualListModel<Permission>(source, target);
 		return this.pickEditPerms;
 	}
-	public void setPickEditPerms(DualListModel<Permission> pickEditPerms) {
-		this.pickEditPerms = pickEditPerms;
-	}
-	public GroupeCtrl(){
-		this.dao = DAOFactory.getGroupeDAO();
-		this.pdao = DAOFactory.getPermissionDAO();
-		this.groupe = new Groupe();
-		this.groupeEdit = new Groupe();
-		this.groupes = new ListDataModel<Groupe>();
-		this.groupes.setWrappedData(dao.all());
-		
-	}
-	public Groupe getGroupeEdit() {
-		return groupeEdit;
-	}
-	public void setGroupeEdit(Groupe groupeEdit) {
-		this.groupeEdit = groupeEdit;
-	}
-	public Groupe getGroupe() {
-		return groupe;
-	}
-	public void setGroupe(Groupe groupe) {
-		this.groupe = groupe;
-	}
-	public ListDataModel<Groupe> getGroupes() {
-		return groupes;
-	}
-	public void setGroupes(ListDataModel<Groupe> groupes) {
-		this.groupes = groupes;
-	}
-	@Override
-	public List<Groupe> getFiltered() {
-		return this.filtered;
+
+	public String goCreate() {
+		return "goCreateGroup";
 	}
 
 	@Override
-	public void setFiltered(List<Groupe> list) {
-		this.filtered = list;
+	public void onCancel(RowEditEvent event) {
+		// TODO Auto-generated method stub
+
 	}
 
 	@Override
@@ -99,18 +136,47 @@ public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>,
 		this.dao.save((Groupe) event.getObject());
 	}
 
-	@Override
-	public void onCancel(RowEditEvent event) {
-		// TODO Auto-generated method stub
-		
+	public void onTransfer(TransferEvent event) {
+		List<Permission> p = this.groupe.getPermissions();
+		for (Object item : event.getItems()) {
+			Permission perm = (Permission) item;
+			p.add(perm);
+		}
+		this.groupe.setPermissions(p);
+	}
+
+	public void onTransferEdit(TransferEvent event) {
+		List<Permission> p = this.groupeEdit.getPermissions();
+		for (Object item : event.getItems()) {
+			Permission perm = (Permission) item;
+			p.add(perm);
+		}
+		this.groupeEdit.setPermissions(p);
 	}
 
 	@Override
-	public String delete() {
-		Groupe g = (Groupe) this.groupes.getRowData();
-		this.dao.delete(g);
-		this.groupes.setWrappedData(dao.all());
-		return "goGrpList";
+	public void setFiltered(List<Groupe> list) {
+		this.filtered = list;
+	}
+
+	public void setGroupe(Groupe groupe) {
+		this.groupe = groupe;
+	}
+
+	public void setGroupeEdit(Groupe groupeEdit) {
+		this.groupeEdit = groupeEdit;
+	}
+
+	public void setGroupes(ListDataModel<Groupe> groupes) {
+		this.groupes = groupes;
+	}
+
+	public void setPickCreatePerms(DualListModel<Permission> pickCreatePerms) {
+		this.pickCreatePerms = pickCreatePerms;
+	}
+
+	public void setPickEditPerms(DualListModel<Permission> pickEditPerms) {
+		this.pickEditPerms = pickEditPerms;
 	}
 
 	@Override
@@ -118,39 +184,5 @@ public class GroupeCtrl implements Controlable, RowEditable, Filterable<Groupe>,
 		this.dao.save(this.groupeEdit);
 		this.groupes.setWrappedData(dao.all());
 		return "goGrpList";
-	}
-
-	@Override
-	public String create() {
-		Iterator<Permission> i = this.groupe.getPermissions().iterator();
-		while(i.hasNext()){
-			Permission permission = i.next();
-			System.out.println("saving:"+permission.getId().toString()+"//"+permission.getValeur());
-		}
-		this.dao.save(this.groupe);
-		this.groupe = new Groupe();
-		this.groupes.setWrappedData(dao.all());
-		return "goGrpList";
-	}
-	public void onTransferEdit(TransferEvent event) { 
-		List<Permission> p = this.groupeEdit.getPermissions();
-		for(Object item : event.getItems()){
-			Permission perm = (Permission) item;
-			p.add(perm);
-		}
-		this.groupeEdit.setPermissions(p);
-	}
-	public void onTransfer(TransferEvent event) { 
-		List<Permission> p = this.groupe.getPermissions();
-		for(Object item : event.getItems()){
-			Permission perm = (Permission) item;
-			p.add(perm);
-		}
-		this.groupe.setPermissions(p);
-	}
-	@Override
-	public String edit() {
-		this.groupeEdit = (Groupe) this.groupes.getRowData();
-		return "goModGroup";
 	}
 }
--- lafay/src/be/lreenaers/lafay/web/control/PermissionCtrl.java
+++ lafay/src/be/lreenaers/lafay/web/control/PermissionCtrl.java
@@ -23,36 +23,39 @@ import be.lreenaers.lafay.web.interfaces.RowEditable;
 
 /**
  * @author ludo
- *
+ * 
  */
-@ManagedBean(name="permissionCtrl")
+@ManagedBean(name = "permissionCtrl")
 @SessionScoped
-public class PermissionCtrl implements Controlable, RowEditable, Filterable<Permission>{
-	// TODO:  implements Controlable, RowEditable for other COntrolers
+public class PermissionCtrl implements Controlable, RowEditable,
+		Filterable<Permission> {
+	// TODO: implements Controlable, RowEditable for other COntrolers
 	private PermissionDAO dao;
 	private GroupeDAO gdao;
 	private ListDataModel<Permission> permissions;
 	private Permission permission;
 	private Permission permissionEdit;
 	private List<Permission> filtered;
-	
 
-	public PermissionCtrl(){
+	public PermissionCtrl() {
 		this.dao = DAOFactory.getPermissionDAO();
-		this.gdao = DAOFactory.getGroupeDAO();
 		this.permissions = new ListDataModel<Permission>();
 		this.permissions.setWrappedData(this.dao.all());
 		this.permission = new Permission();
 		this.permissionEdit = new Permission();
-		
+
 	}
-	public String delete(){
+
+	public String delete() {
+		this.gdao = DAOFactory.getGroupeDAO();
 		Permission p = (Permission) permissions.getRowData();
 		List<Groupe> groups = gdao.all();
 		Iterator<Groupe> it = groups.iterator();
-		while (it.hasNext()){
-			Groupe g =it.next();
-			List<Permission> perms = g.getPermissions();
+		Groupe g;
+		List<Permission> perms;
+		while (it.hasNext()) {
+			g = it.next();
+			perms = g.getPermissions();
 			perms.remove(p);
 			g.setPermissions(perms);
 			this.gdao.save(g);
@@ -61,56 +64,69 @@ public class PermissionCtrl implements Controlable, RowEditable, Filterable<Perm
 		this.permissions.setWrappedData(this.dao.all());
 		return "goPermList";
 	}
-	public String update(){
+
+	public String update() {
 		this.dao.save(this.permissionEdit);
 		this.permissions.setWrappedData(this.dao.all());
 		return "goPermList";
 	}
-	public String create(){
+
+	public String create() {
 		this.dao.save(this.permission);
 		this.permission = new Permission();
 		this.permissions.setWrappedData(this.dao.all());
 		return "goPermList";
 	}
-	public void onEdit(RowEditEvent event){
-		 this.dao.save((Permission) event.getObject());
-		 //this.update();
+
+	public void onEdit(RowEditEvent event) {
+		this.dao.save((Permission) event.getObject());
+		// this.update();
 	}
-	public void onCancel(RowEditEvent event){
-		
+
+	public void onCancel(RowEditEvent event) {
+
 	}
-	 public String goCreate(){
-		 return "goCreatePerm";
-	 }
-	
+
+	public String goCreate() {
+		return "goCreatePerm";
+	}
+
 	public PermissionDAO getDao() {
 		return dao;
 	}
+
 	public void setDao(PermissionDAO dao) {
 		this.dao = dao;
 	}
+
 	public ListDataModel<Permission> getPermissions() {
 		return this.permissions;
 	}
+
 	public void setPermissions(ListDataModel<Permission> permissions) {
 		this.permissions = permissions;
 	}
+
 	public Permission getPermission() {
 		return permission;
 	}
+
 	public void setPermission(Permission permission) {
 		this.permission = permission;
 	}
+
 	public Permission getPermissionEdit() {
 		return permissionEdit;
 	}
+
 	public void setPermissionEdit(Permission permissionEdit) {
 		this.permissionEdit = permissionEdit;
 	}
-	
+
 	public List<Permission> getFiltered() {
 		return filtered;
 	}
+
 	public void setFiltered(List<Permission> filtered) {
 		this.filtered = filtered;
 	}
