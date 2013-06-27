9d3c361c6f2101f2aacc1266283c23d2a965d9f9
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2013-06-27 19:42:05 +0200

    khg

--- lafay/src/be/lreenaers/lafay/beans/Enchainable.java
+++ lafay/src/be/lreenaers/lafay/beans/Enchainable.java
@@ -18,10 +18,11 @@ public class Enchainable extends Bean {
 	private static final long serialVersionUID = 1L;
 	@Reference
 	private Exercice exercice;
+	private String nom;
 	private int repetitionMinimum;
 	private int repetitionEffective;
 	private int dureeEnSeconde;
-	private boolean compteur;
+	private boolean pause;
 
 	@Override
 	public boolean equals(Object obj) {
@@ -32,14 +33,19 @@ public class Enchainable extends Bean {
 		if (getClass() != obj.getClass())
 			return false;
 		Enchainable other = (Enchainable) obj;
-		if (compteur != other.compteur)
-			return false;
 		if (dureeEnSeconde != other.dureeEnSeconde)
 			return false;
 		if (exercice == null) {
 			if (other.exercice != null)
 				return false;
 		} else if (!exercice.equals(other.exercice))
+			return false;
+		if (nom == null) {
+			if (other.nom != null)
+				return false;
+		} else if (!nom.equals(other.nom))
+			return false;
+		if (pause != other.pause)
 			return false;
 		if (repetitionEffective != other.repetitionEffective)
 			return false;
@@ -56,6 +62,10 @@ public class Enchainable extends Bean {
 		return exercice;
 	}
 
+	public String getNom() {
+		return nom;
+	}
+
 	public int getRepetitionEffective() {
 		return repetitionEffective;
 	}
@@ -68,23 +78,18 @@ public class Enchainable extends Bean {
 	public int hashCode() {
 		final int prime = 31;
 		int result = super.hashCode();
-		result = prime * result + (compteur ? 1231 : 1237);
 		result = prime * result + dureeEnSeconde;
 		result = prime * result
 				+ ((exercice == null) ? 0 : exercice.hashCode());
+		result = prime * result + ((nom == null) ? 0 : nom.hashCode());
+		result = prime * result + (pause ? 1231 : 1237);
 		result = prime * result + repetitionEffective;
 		result = prime * result + repetitionMinimum;
 		return result;
 	}
 
-	public boolean isCompteur() {
-		// <h:commandLink id="link" action="navigationPageX"
-		// rendered="#{sessionController.isTypeA}">
-		return compteur;
-	}
-
-	public void setCompteur(boolean compteur) {
-		this.compteur = compteur;
+	public boolean isPause() {
+		return pause;
 	}
 
 	public void setDureeEnSeconde(int dureeEnSeconde) {
@@ -93,6 +98,14 @@ public class Enchainable extends Bean {
 
 	public void setExercice(Exercice exercice) {
 		this.exercice = exercice;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public void setPause(boolean pause) {
+		this.pause = pause;
 	}
 
 	public void setRepetitionEffective(int repetitionEffective) {
