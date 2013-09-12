6fa30d42b78eecf5e3b4d207a76c4170efb57863
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2013-09-12 14:41:28 +0200

    created Repos UI

---
+++ lafay/src/be/lreenaers/lafay/web/controls/ReposCtrl.java
@@ -0,0 +1,23 @@
+package be.lreenaers.lafay.web.controls;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import be.lreenaers.lafay.beans.Repos;
+import be.lreenaers.lafay.factories.DAOFactory;
+
+@ManagedBean(name = "reposCtrl")
+@SessionScoped
+public class ReposCtrl extends Controler<Repos> {
+
+	public ReposCtrl() {
+		super(Repos.class);
+		this.entityClassList.setWrappedData(this.dao.all());
+	}
+
+	@Override
+	public void selectDAO() {
+		this.dao = DAOFactory.getReposDAO();
+
+	}
+
+}
